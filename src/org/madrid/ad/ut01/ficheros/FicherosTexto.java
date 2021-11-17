b46de8bcb6910cd1c9f08c3464c1b3de9e652895
josevinas <80360344+josevinas@users.noreply.github.com>
2021-11-17 10:04:00 +0100

    Update FicherosTexto.java

--- src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
+++ src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
@@ -217,8 +217,50 @@ public class FicherosTexto implements InterfazFicherosTexto{
 
 	@Override
 	public int palabraMasLarga(String rutaFichero) {
-		// TODO Auto-generated method stub
-		return 0;
+		String palabraLarga = "";
+		BufferedReader br = null;
+
+		try {
+			String linea;
+
+			String palabraComparar;
+
+			br = new BufferedReader(new FileReader(rutaFichero));
+
+			String[] palabras;
+
+			while ((linea = br.readLine()) != null) {
+
+				linea = linea.replaceAll("[., ,, ;, :, -, ¿, ?, !, ¡]", " ");
+
+				palabras = linea.split(" ");
+
+				for (int i = 0; i < palabras.length; i++) {
+
+					palabraComparar = palabras[i];
+
+					if (palabraComparar.length() > palabraLarga.length()) {
+						palabraLarga = palabraComparar;
+					}
+				}
+			}
+
+			System.out.println(
+					"La palabra más larga es " + palabraLarga + " y tiene " + palabraLarga.length() + " caracteres.");
+
+		} catch (FileNotFoundException e) {
+			System.err.println("El fichero no ha sido encontrado");
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+
+		return 1;
 	}
 
 	@Override
